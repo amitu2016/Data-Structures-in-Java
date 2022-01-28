71ca2e508445e4f5945f46e65097e6acc112bba3
noobcoder <amitu2016@gmail.com>
2022-01-28 21:22:00 +0530

    recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,19 @@
+public class Factorial {
+
+    public static int factorial(int n) {
+        if(n < 0){
+            return -1;
+        }
+
+        if(n ==0 || n == 1){
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int result = factorial(10);
+        System.out.println(result);
+    }
+}
